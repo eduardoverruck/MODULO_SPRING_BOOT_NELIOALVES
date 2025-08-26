8d4115c01a7926a97320774ffa55765cbc219481
eduardoverruck <verruckeduardo@gmail.com>
2025-08-26 20:25:12 +0100

    foi criado o retorno dos users em lista

--- src/main/java/com/vrk/course/resources/UserResource.java
+++ src/main/java/com/vrk/course/resources/UserResource.java
@@ -1,6 +1,8 @@
 package com.vrk.course.resources;
 
 import com.vrk.course.entities.User;
+import java.util.ArrayList;
+import java.util.List;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,14 +13,23 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserResource {
     
     @GetMapping
-    public ResponseEntity<User> findAll(){
+    public ResponseEntity<List<User>> findAll(){
         
         User u1 = new User(1L, "jao", "jao@gamil", "9074390754", "12345687");
         User u2 = new User(2L, "jao", "jao@gamil", "9074390754", "12345687");
         User u3  = new User(3L, "jao", "jao@gamil", "9074390754", "12345687");
         User u4 = new User(3L, "jao", "jao@gamil", "9074390754", "12345687");
         
-        return ResponseEntity.ok().body(u1);
+        List<User> listaDeUsers = new ArrayList<>();
+        
+        listaDeUsers.add(u1);
+        listaDeUsers.add(u2);
+        listaDeUsers.add(u3);
+        listaDeUsers.add(u4);
+        
+        System.out.println(listaDeUsers);
+        
+        return ResponseEntity.ok().body(listaDeUsers);
         
     }
     
